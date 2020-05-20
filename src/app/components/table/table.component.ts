3a731208917153ff4d190b27f157c6d598987d33
Gorev <ogogorev@yandex.ru>
2020-05-20 17:02:02 +0200

    Update, Add comment

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -32,6 +32,29 @@ export class TableComponent implements OnInit {
     console.log('TABLE update loading');
 
     this.loading = loading;
+
+    /**
+     * 
+     * If we send loadingChange event synchronously (without setTimeout),
+     * it will produce an ExpressionChangedAfterItHasBeenCheckedError.
+     * 
+     * As I understand, following events are happening:
+     * 1. (search) Search button in the search.component is clicked
+     * 2. (search) the searchString is changed in the handler method 
+     * 3. (table) new value of searchString passed into table.component
+     * 4. (table) new data request is triggered
+     * 5. (table) updateLoading function in the table.component is called
+     * 6. (table) loadingChange event emitted
+     * 7. (search) handler of this event is called
+     * 8. (search) loading var is updated with 'true'
+     * 9. Angular starts checking (probably it is already started somewhere between steps above)
+     * 10. the error is thrown
+     * 
+     * My assumption is that Angular starts checking between steps 2 and 3.
+     * I don't understand why is change detection process started but not finished before the step 3
+     * 
+     */
+
     // setTimeout(() => {
     this.loadingChange.emit(this.loading);
     // }, 0);
